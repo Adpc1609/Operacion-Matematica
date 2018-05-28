ee165d561d0534dc5cfe2d5376a62526f6e94600
Adriana Perez <adpc1609@gmail.com>
2018-05-27 21:57:21 -0400

    funciones Matematicas

---
+++ app.js
@@ -0,0 +1,11 @@
+let add =function(num1,num2){
+    return(num1+num2);
+}
+
+let multiply =function(num1,num2){
+    return(num1*num2);
+}
+
+var resul= multiply(add(9824,777)) {
+    console.log(multiply(36325,add(9824,777))
+}
